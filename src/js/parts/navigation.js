8e937351effd7de7b41b7f4bcd9ecc010d46fb3f
Uzurch1k <galcenkovlad7@gmail.com>
2024-12-03 21:33:42 +0200

    add footer

--- src/js/parts/navigation.js
+++ src/js/parts/navigation.js
@@ -2,6 +2,8 @@ import { toggle, up } from 'slide-element';
 
 function handleMenuItemChildren() {
   const mobMenu = document.querySelector('.mobmenu');
+  const footerList = document.querySelector('.footer__list');
+  const footerSubMenu = document.querySelectorAll('.footer__list .sub-menu');
 
   const isMobile = () => window.innerWidth < 960;
 
@@ -13,11 +15,21 @@ function handleMenuItemChildren() {
     }
   }
 
+  if (footerList) {
+    if (isMobile()) {
+      activateFooterMenu(footerList);
+    } else {
+      deactivateFooterMenu(footerList);
+    }
+  }
+
   window.addEventListener('resize', () => {
     if (isMobile()) {
-      activateMobileMenu(mobMenu);
+      if (mobMenu) activateMobileMenu(mobMenu);
+      if (footerList) activateFooterMenu(footerList);
     } else {
-      deactivateMobileMenu(mobMenu);
+      if (mobMenu) deactivateMobileMenu(mobMenu);
+      if (footerList) deactivateFooterMenu(footerList);
     }
   });
 
@@ -32,6 +44,23 @@ function handleMenuItemChildren() {
     if (menuElement.hasEventListener) {
       menuElement.removeEventListener('click', openNavItem);
       menuElement.hasEventListener = false; // Сбрасываем флаг
+    }
+  }
+
+  function activateFooterMenu(listElement) {
+    if (!listElement.hasEventListener) {
+      listElement.addEventListener('click', openFooterItem);
+      listElement.hasEventListener = true; // Флаг для предотвращения дублирования
+    }
+  }
+
+  function deactivateFooterMenu(listElement) {
+    if (listElement.hasEventListener) {
+      listElement.removeEventListener('click', openFooterItem);
+      listElement.hasEventListener = false; // Сбрасываем флаг
+      footerSubMenu.forEach(ul => {
+        ul.style.display = '';
+      });
     }
   }
 
@@ -62,7 +91,33 @@ function handleMenuItemChildren() {
       });
     }
   }
+
+  function openFooterItem(event) {
+    const target = event.target;
+    const footerItem = target.closest('.menu-item-has-children');
+    const footerItemLink = target.closest('.menu-item-has-children > a');
+
+    if (footerItem && footerItemLink === target) {
+      event.preventDefault();
+
+      const subNav = footerItem.querySelector('.sub-menu');
+
+      toggle(subNav, { display: 'flex' });
+
+      footerItem.classList.toggle('isOpened');
+
+      const allFooterItemsOpened = footerList.querySelectorAll(
+        '.menu-item-has-children.isOpened'
+      );
+      allFooterItemsOpened.forEach(item => {
+        if (item !== footerItem) {
+          const subNavOth = item.querySelector('.sub-menu');
+          up(subNavOth);
+          item.classList.remove('isOpened');
+        }
+      });
+    }
+  }
 }
 
-// Инициализация на событие DOMContentLoaded
 document.addEventListener('DOMContentLoaded', handleMenuItemChildren);
