e9e865229490bd83ef70fdbcb2b874d3d0afd161
Uzurch1k <galcenkovlad7@gmail.com>
2024-12-07 18:17:21 +0200

    fix

--- src/js/parts/tools.js
+++ src/js/parts/tools.js
@@ -1,68 +1,32 @@
 // Visibil elem mob or desc =========================
 
-const elemDesc = document.querySelectorAll('.el-desc');
-const elemMob = document.querySelectorAll('.el-mob');
+// const elemDesc = document.querySelectorAll('.el-desc');
+// const elemMob = document.querySelectorAll('.el-mob');
 
-function visibilElemMobOrDesc() {
-  const screenWidth = window.innerWidth > 960;
+// function visibilElemMobOrDesc() {
+//   const screenWidth = window.innerWidth > 960;
 
-  elemDesc?.forEach(el => {
-    if (screenWidth) {
-      el.style.display = '';
-    } else {
-      el.style.display = 'none';
-    }
-  });
+//   elemDesc?.forEach(el => {
+//     if (screenWidth) {
+//       el.style.display = '';
+//     } else {
+//       el.style.display = 'none';
+//     }
+//   });
 
-  elemMob?.forEach(el => {
-    if (screenWidth) {
-      el.style.display = 'none';
-    } else {
-      el.style.display = '';
-    }
-  });
-}
-
-// Update main padding top =========================
-
-const headerMain = document.querySelector('.header__main');
-const main = document.querySelector('main');
-
-function updateMainPaddingTop() {
-  if (headerMain && main) {
-    const headerHeight = headerMain.getBoundingClientRect().height;
-    main.style.paddingTop = `${headerHeight}px`;
-  }
-}
-
-// Update min-height first sec =========================
-
-const fullscreenSec = document.querySelectorAll('.fullscreen');
-
-function updateMinHeightFirstSec() {
-  if (headerMain && fullscreenSec) {
-    const screenWidth = window.innerWidth > 960;
-
-    fullscreenSec.forEach(section => {
-      const headerHeight = headerMain.getBoundingClientRect().height;
-
-      if (!section.classList.contains('halvscr')) {
-        if (screenWidth) {
-          section.style.minHeight = `calc(100vh - ${headerHeight}px)`;
-        } else {
-          section.style.minHeight = '';
-        }
-      }
-    });
-  }
-}
+//   elemMob?.forEach(el => {
+//     if (screenWidth) {
+//       el.style.display = 'none';
+//     } else {
+//       el.style.display = '';
+//     }
+//   });
+// }
 
 // Init tools =========================
 
 function toolsInit() {
-  updateMainPaddingTop();
-  visibilElemMobOrDesc();
-  updateMinHeightFirstSec();
+  // visibilElemMobOrDesc();
 }
 
 window.addEventListener('resize', toolsInit);
